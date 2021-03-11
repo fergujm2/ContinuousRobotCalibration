e9e662cafa3b7a89c28151c3bef4c283f3927fb1
James <james.m.ferguson@vanderbilt.edu>
2021-03-11 15:55:34 -0600

    Change figures a bit

--- Figures/BSplines/GenerateBSplineExample.m
+++ Figures/BSplines/GenerateBSplineExample.m
@@ -20,14 +20,22 @@ clf;
 h.Color = [1,1,1];
 hold on;
 
+rgbTable = 1./255.*[237,  32,  36;
+                    247, 148,  32;
+                    247, 236,  19;
+                    106, 189,  69;
+                     57,  83, 164;
+                    102,  47, 144;
+                    255,   0, 255];
+            
 for iii = 1:n
     xi = sval2(d, y, C(iii,:), t);
-    plot(t, xi);
+    plot(t, xi, 'Color', rgbTable(iii,:), 'LineWidth', 0.5);
 end
 
 c = [1.4, 1.9, 1, 0.9, 1.7, 1.3, 1.9];
 x = sval2(d, y, c, t);
-plot(t, x, '-k', 'LineWidth', 1);
+plot(t, x, '-k', 'LineWidth', 0.75);
     
 xlabel('$t$', 'interpreter', 'latex');
 ylabel('$f(t)$', 'interpreter', 'latex');
